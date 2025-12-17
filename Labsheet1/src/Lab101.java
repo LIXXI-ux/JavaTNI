1a31feb854beb41bc9f1cc5781fc34535df6b882
LIXXI-ux <Areyounikro1@gmail.com>
2025-12-17 12:45:21 +0700

    Labsheet1 2513510137

---
+++ Labsheet1/src/Lab101.java
@@ -0,0 +1,7 @@
+public class Lab101 {
+    public static void main(String[] args) {
+        System.out.println("I am Komcharn");
+        System.out.println("\tThis is my first time to learn  Java");
+        System.out.println("\tI hope to get a grade \"A\"");
+    }
+}
